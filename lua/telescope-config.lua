e1d979e271f57402dcda235e55ffa83e137ffb55
Albin Groen <albin.groen@gmail.com>
2021-11-20 17:09:13 +0100

    :sparkles: Update Telescope layout to Ivy

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,4 +1,14 @@
-require("telescope").load_extension("git_worktree")
+local telescope = require("telescope")
+
+telescope.setup{
+  pickers = {
+    find_files = {
+      theme = "ivy",
+    }
+  }
+}
+
+telescope.load_extension("git_worktree")
 
 vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
