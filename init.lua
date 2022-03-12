5476d677e018431184edfa24eedeb2531e128af0
Infinitybeond1 <88919270+Infinitybeond1@users.noreply.github.com>
2022-03-11 18:36:30 -0600

    replace blackcaldrons surround.nvim with a working version
    The original suuround.vim was not working, so i replaced it with a fork that works perfectly

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ return require('packer').startup(function()
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use {
-    "blackCauldron7/surround.nvim",
+    "ur4ltz/surround.nvim,
     config = function()
       require"surround".setup {mappings_style = "surround"}
     end
