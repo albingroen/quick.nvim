5c974cd3d0b0627afc9cb528c3781c2dc2ea7bb8
Tommy TIAN <xtianae@connect.ust.hk>
2022-06-25 13:54:03 +0000

    modify theme

--- init.lua
+++ init.lua
@@ -30,15 +30,6 @@ return require('packer').startup(function()
   
     -- Change the "hint" color to the "orange" color, and make the "error" color bright red
     colors = {hint = "orange", error = "#ff0000"},
-  
-    -- Overwrite the highlight groups
-    -- overrides = function(c)
-    --   return {
-    --     htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-    --     DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-    --     -- this will remove the highlight groups
-    --     TSField = {},
-    --   }
     end
   })
 
