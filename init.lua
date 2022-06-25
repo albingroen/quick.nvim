ff1b3000c95d25db337fcf8ffd8000fa26a71ba9
Tommy TIAN <xtianae@connect.ust.hk>
2022-06-25 13:56:49 +0000

    remove theme

--- init.lua
+++ init.lua
@@ -21,26 +21,7 @@ require'nvim-treesitter.configs'.setup {
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use {'neoclide/coc.nvim', branch = 'release'}
-  -- use 'folke/tokyonight.nvim'
-  -- github theme
-  require("github-theme").setup({
-    theme_style = "dark",
-    function_style = "italic",
-    -- sidebars = {"qf", "vista_kind", "terminal", "packer"},
-  
-    -- -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-    -- colors = {hint = "orange", error = "#ff0000"},
-  
-    -- -- Overwrite the highlight groups
-    -- overrides = function(c)
-    --   return {
-    --     htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-    --     DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-    --     -- this will remove the highlight groups
-    --     TSField = {},
-    --   }
-    -- end
-  })
+)
 
   use 'nvim-treesitter/nvim-treesitter' 
   use 'tpope/vim-commentary'
--- lua/colors.lua
+++ lua/colors.lua
@@ -1,3 +1,3 @@
-vim.g.tokyonight_style = 'night'
-vim.cmd([[colorscheme tokyonight]])
-vim.o.background = 'dark'
+-- vim.g.tokyonight_style = 'night'
+-- vim.cmd([[colorscheme tokyonight]])
+-- vim.o.background = 'dark'
