2949cae3e42f40b409da10867a08d122ba08aad9
Tommy TIAN <xtianae@connect.ust.hk>
2022-06-25 13:55:58 +0000

    modify theme

--- init.lua
+++ init.lua
@@ -28,9 +28,18 @@ return require('packer').startup(function()
     function_style = "italic",
     -- sidebars = {"qf", "vista_kind", "terminal", "packer"},
   
-    -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-    colors = {hint = "orange", error = "#ff0000"},
-    end
+    -- -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+    -- colors = {hint = "orange", error = "#ff0000"},
+  
+    -- -- Overwrite the highlight groups
+    -- overrides = function(c)
+    --   return {
+    --     htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+    --     DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+    --     -- this will remove the highlight groups
+    --     TSField = {},
+    --   }
+    -- end
   })
 
   use 'nvim-treesitter/nvim-treesitter' 
