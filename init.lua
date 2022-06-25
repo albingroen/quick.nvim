b205f22f0b9756e1ae1e81bfc9dce0be9c4455d3
Tommy TIAN <xtianae@connect.ust.hk>
2022-06-25 13:51:34 +0000

    modify theme

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ return require('packer').startup(function()
   require("github-theme").setup({
     theme_style = "dark",
     function_style = "italic",
-    sidebars = {"qf", "vista_kind", "terminal", "packer"},
+    -- sidebars = {"qf", "vista_kind", "terminal", "packer"},
   
     -- Change the "hint" color to the "orange" color, and make the "error" color bright red
     colors = {hint = "orange", error = "#ff0000"},
