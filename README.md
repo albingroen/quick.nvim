b95b3563d4410919f452fbe130e98c4d371cae8d
Albin Groen <albin.groen@gmail.com>
2023-12-08 17:00:18 +0100

    feat: implement native neovim LSP

--- README.md
+++ README.md
@@ -1,48 +1,52 @@
 # quick.nvim
 
-[YouTube Demo](https://www.youtube.com/watch?v=OhbgZbORFd4)
-
-A very fast Lua based Neovim configuration that uses coc.nvim for intellisense
+A very fast Lua based Neovim configuration that uses native LSP for intellisense
 
 - [What is this?](#what-is-this)
 - [Plugins](#plugins)
 - [Prerequisites](#prerequisites)
 - [Setup](#setup-quicknvim)
-  - [Download cofiguration](#1-download-configuration)
+  - [Download configuration](#1-download-configuration)
     - [Releases](#releases)
   - [Install plugins](#2-install-plugins)
-    - [Install Packer](#install-packer)
-- [coc.nvim](#cocnvim)
-- [Bindings](#bindings)
 - [Maintainers](#maintainers)
 
 ## What is this?
 
-This is a configuration for Neovim. It's written in Lua, and uses modern
-plugins like Treesitter & Telescope. It does however not implement
-the native Neovim LSP. It's very fast, good looking, and has a lot of nice
-features.
+This is a configuration for Neovim. It's written in Lua, and uses modern plugins like Treesitter & Telescope. It also implements the native Neovim LSP. It's very fast, good looking, and has a lot of nice features.
 
-<img width="1280" alt="quick nvim" src="https://user-images.githubusercontent.com/19674362/218315573-d6ee5f66-653d-4501-a463-36bf431a0378.png">
-
-https://user-images.githubusercontent.com/19674362/139526856-bdd9023d-8780-4b0f-aa4b-4edb619e35fd.mp4
+![quick-nvim-min](https://github.com/albingroen/quick.nvim/assets/19674362/3721f3a0-7198-4e23-9d76-a907dadb91ff)
 
 ## Plugins
 
-- [Packer](https://github.com/wbthomason/packer.nvim)
-- [Plenary](https://github.com/nvim-lua/plenary.nvim)
-- [Surround](https://github.com/url4ltz/surround.nvim)
+- [vim-sleuth](https://github.com/tpope/vim-sleuth)
+- [vim-rhubarb](https://github.com/tpope/vim-rhubarb)
+- [vim-fugitive](https://github.com/tpope/vim-fugitive)
 - [Telescope](https://github.com/nvim-telescope/telescope.nvim)
-- [rsms.nvim](https://github.com/kvrohit/rasmus.nvim)
-- [Treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
-- [coc](https://github.com/neoclide/coc.nvim)
+- [vim-kitty-navigator](https://github.com/knubie/vim-kitty-navigator)
+- [nvim-bqf](https://github.com/kevinhwang91/nvim-bqf)
+- [nvim-ts-autotag](https://github.com/windwp/nvim-ts-autotag)
+- [Comment.nvim](https://github.com/numToStr/Comment.nvim)
 - [nvim-ts-context-commentstring](https://github.com/JoosepAlviste/nvim-ts-context-commentstring)
-- [vim-commentary](https://github.com/tpope/vim-commentary/)
+- [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
+- [mason.nvim](https://github.com/williamboman/mason.nvim)
+- [mason-lspconfig.nvim](https://github.com/williamboman/mason-lspconfig.nvim)
+- [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
+- [LuaSnip](https://github.com/L3MON4D3/LuaSnip)
+- [cmp_luasnip](https://github.com/saadparwaiz1/cmp_luasnip)
+- [rasmus.nvim](https://github.com/kvrohit/rasmus.nvim)
+- [cmp-nvim-lsp](https://github.com/hrsh7th/cmp-nvim-lsp)
+- [cmp-buffer](https://github.com/hrsh7th/cmp-buffer)
+- [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+- [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
+- [rasmus.nvim](https://github.com/kvrohit/rasmus.nvim)
+- [conform.nvim](https://github.com/stevearc/conform.nvim)
+- [nvim-autopairs](https://github.com/windwp/nvim-autopairs)
+- [gitsigns.nvim](https://github.com/lewis6991/gitsigns.nvim)
 
 ## Prerequisites
 
-- [Neovim 0.5 or above](https://neovim.io)
-- [Node.js](https://nodejs.org)
+- [Neovim 0.9 or above](https://neovim.io)
 
 ## Setup quick.nvim
 
@@ -50,9 +54,7 @@ https://user-images.githubusercontent.com/19674362/139526856-bdd9023d-8780-4b0f-
 
 #### Clone repository
 
-If you want to receive updates as this configuration gets patched, then clone
-this repository into your `.config` directory, or wherever you keep your
-current Neovim configuration.
+If you want to receive updates as this configuration gets patched, then clone this repository into your `.config` directory, or wherever you keep your current Neovim configuration.
 
 ```sh
 git clone git@github.com:albingroen/quick.nvim.git ~/.config/nvim
@@ -60,94 +62,38 @@ git clone git@github.com:albingroen/quick.nvim.git ~/.config/nvim
 
 #### Releases
 
-On the other hand, if you want a raw folder with the configuration files,
-without git connected to it, you can head over to the releases page, and
-download a release from there.
+On the other hand, if you want a raw folder with the configuration files, without git connected to it, you can head over to the releases page, and download a release from there.
 
 https://github.com/albingroen/quick.nvim/releases
 
 ### 2. Install plugins
 
-#### Install Packer
-
-quick.nvim uses [Packer](https://github.com/wbthomason/packer.nvim) as its
-Neovim plugin manager.
-
-```
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-To install the plugins used by quick.nvim, you need to do so using Packer.
+To install the plugins used by quick.nvim, you need to do so using Lazy.
 
 ```
 nvim ~/.config/nvim/init.lua
 ```
 
 ```
-:PackerInstall
+:Lazy sync
 ```
-
-PS. If you're having trouble running `:PackerInstall` after this, please follow the instructions in this issue: https://github.com/albingroen/quick.nvim/issues/2
 
 ### Install ripgrep
 
-In order for the Telescope live_grep functionality to work, you need to have
-ripgrep installed on your machine. Please reference the official ripgrep
-installation options, but if run Homebrew, you can install it like this.
+In order for the Telescope live_grep functionality to work, you need to have ripgrep installed on your machine. Please reference the official ripgrep installation options, but if run Homebrew, you can install it like this.
+
+### Install LSP servers, formatters and linters
+
+You can install anything you want by running `:Mason`. Here are a few that I usually install.
+
+- emmet-language-server emmet_language_server
+- eslint-lsp eslint
+- stylua
+- typescript-language-server tsserver
 
 ```
 brew install rg
 ```
-
-## coc.nvim
-
-This configuration uses [coc.nvim](https://github.com/neoclide/coc.nvim) for
-intellisense, instead of the built-in Neovim LSP. This is because I think it's
-not quite mature yet, pretty tricky to configure, and doesn't have a quite as
-many languages and frameworks supported.
-
-quick.nvim doesn't ship with any default coc plugins though, so you have to
-install the ones you want to use. For example if you want to use quick.nvim for
-TypeScript programming, you'll have to install the coc-tsserver plugin like
-this.
-
-```
-:CocInstall coc-tsserver
-```
-
-## Bindings
-
-**\<leader\>** = "\<Space/\>"
-
-| Plugin    | Mapping      | Action                         |
-| --------- | ------------ | ------------------------------ |
-|           | \<C-H\>      | Move one split left            |
-|           | \<C-J\>      | Move one split down            |
-|           | \<C-K\>      | Move one split up              |
-|           | \<C-L\>      | Move one split right           |
-|           | \<C-N\>      | Open file explorer             |
-|           | \<C-S\>      | Open search & replace          |
-|           | \<leader-t\> | Open a terminal buffer         |
-|           | sp           | Split window horizontally      |
-|           | tj           | Move one tab left              |
-|           | tk           | Move one tab right             |
-|           | tn           | Create a new tab               |
-|           | to           | Close all other tabs           |
-|           | vs           | Split window vertically        |
-| coc       | K            | Hover over symbol              |
-| coc       | \<C-Space\>  | Open autocompletion            |
-| coc       | \<CR\>       | Select autocompletion          |
-| coc       | \<S-TAB\>    | Browse previous autocompletion |
-| coc       | \<TAB\>      | Browse next autocompletion     |
-| coc       | \<leader\>.  | Open code actions              |
-| coc       | \<leader\>f  | Format file with prettier      |
-| coc       | \<leader\>l  | Execute code autofix           |
-| coc       | \<leader\>rn | Rename symbol                  |
-| coc       | gd           | Go to definition               |
-| Telescope | \<C-B\>      | Open buffer picker             |
-| Telescope | \<C-F\>      | Open project search            |
-| Telescope | \<C-P\>      | Open file picker               |
 
 ## Maintainers
 
--- init.lua
+++ init.lua
@@ -1,39 +1,340 @@
-require('basics')
-require('colors')
-require('telescope-config')
-require('coc-config')
+-- Basic settings
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.mouse = "a"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.hlsearch = false
+vim.o.showmode = false
+vim.o.swapfile = false
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.termguicolors = true
+vim.o.signcolumn = "yes"
+vim.o.hidden = true
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.opt.list = true
+vim.o.laststatus = 3
+vim.o.pumheight = 10
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-  ignore_install = { "phpdoc" },
-  context_commentstring = {
-    enable = true
-  },
-  highlight = {
-    enable = true,
-    disable = { "lua" }
-  },
-  indent = {
-    enable = true
-  }
-}
+-- Basic mappings
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { noremap = true })
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true })
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true })
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { noremap = true })
+vim.keymap.set("n", "<C-S>", ":%s/", { noremap = true })
+vim.keymap.set("n", "sp", ":sp<CR>", { noremap = true })
+vim.keymap.set("n", "tj", ":tabprev<CR>", { noremap = true })
+vim.keymap.set("n", "tk", ":tabnext<CR>", { noremap = true })
+vim.keymap.set("n", "tn", ":tabnew<CR>", { noremap = true })
+vim.keymap.set("n", "to", ":tabo<CR>", { noremap = true })
+vim.keymap.set("n", "vs", ":vs<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>j", ":cnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", ":cprevious<CR>", { noremap = true, silent = true })
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use { 'neoclide/coc.nvim', branch = 'release'}
-  use 'kvrohit/rasmus.nvim'
-  use 'nvim-treesitter/nvim-treesitter' 
-  use 'tpope/vim-commentary'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    "ur4ltz/surround.nvim",
-    config = function()
-      require"surround".setup {mappings_style = "surround"}
-    end
-  }
-end)
+-- Setup lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Install plugins
+require("lazy").setup({
+	-- Automatic indentation
+	"tpope/vim-sleuth",
+
+	-- Git & GitHub plugins
+	"tpope/vim-rhubarb",
+	"tpope/vim-fugitive",
+
+	-- Better navigation between Neovim and Kitty
+	"knubie/vim-kitty-navigator",
+
+	-- Better quickfix lists
+	"kevinhwang91/nvim-bqf",
+
+	-- Autoclose HTML-style tags
+	"windwp/nvim-ts-autotag",
+
+	-- Easy commenting in normal & visual mode
+	{ "numToStr/Comment.nvim", lazy = false },
+	{ "JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy" },
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+	},
+
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		},
+	},
+
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<C-w>", "<cmd>Telescope grep_string<cr>", desc = "Grep string" },
+			{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+			{ "<C-f>", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+
+	-- Better syntax highlighting & much more
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = "all",
+				highlight = { enable = true },
+				indent = { enable = true },
+				autotag = { enable = true, enable_close_on_slash = false },
+			})
+		end,
+	},
+
+	-- Colorscheme
+	{
+		"kvrohit/rasmus.nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			vim.cmd([[colorscheme rasmus]])
+		end,
+	},
+
+	-- Surround words with characters in normal mode
+	{
+		"kylechui/nvim-surround",
+		event = "VeryLazy",
+		opts = {},
+	},
+
+	-- For formatting code
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				lua = { "stylua" },
+			},
+			format_on_save = {},
+		},
+		init = function()
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+
+	-- Pair matching characters
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			disable_filetype = { "TelescopePrompt", "vim" },
+		},
+	},
+
+	-- Gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					-- Actions
+					map("n", "<leader>hs", gs.stage_hunk)
+					map("n", "<leader>hr", gs.reset_hunk)
+					map("v", "<leader>hs", function()
+						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("v", "<leader>hr", function()
+						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("n", "<leader>hS", gs.stage_buffer)
+					map("n", "<leader>hu", gs.undo_stage_hunk)
+					map("n", "<leader>hR", gs.reset_buffer)
+					map("n", "<leader>hp", gs.preview_hunk)
+					map("n", "<leader>hb", function()
+						gs.blame_line({ full = true })
+					end)
+					map("n", "<leader>tb", gs.toggle_current_line_blame)
+					map("n", "<leader>hd", gs.diffthis)
+					map("n", "<leader>hD", function()
+						gs.diffthis("~")
+					end)
+					map("n", "<leader>td", gs.toggle_deleted)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
+})
+
+-- Open Telescope on start
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.argv(0) == "" then
+			require("telescope.builtin").find_files()
+		end
+	end,
+})
+
+-- Set up Comment.nvim
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})
+
+-- Set up Mason and install set up language servers
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
+	end,
+})
+
+-- Global LSP mappings
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+-- More LSP mappings
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>.", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	end,
+})
+
+-- Set up nvim-cmp
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+	},
+})
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
