dbf6557e7dc5e3821e668dac8d90c9834176579c
Albin Groen <albin.groen@gmail.com>
2026-01-09 10:15:44 +0100

    feat: set up tmux navigator

---
+++ lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+	},
+}
