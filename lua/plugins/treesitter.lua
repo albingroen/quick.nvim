875bc66441a163b318765d25bc99803146648b22
Albin Groen <albingroen@proton.me>
2026-01-29 21:50:31 +0100

    fix: fix ts autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	lazy = false,
+	config = true,
 }
